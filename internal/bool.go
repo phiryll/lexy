c3c97ae49e8681035f3b5f4da011cf310ef52785
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-06-10 15:01:31 -0700

    Add initial bool codec implementation.

--- internal/bool.go
+++ internal/bool.go
@@ -1,13 +1,28 @@
 package internal
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type BoolCodec struct{}
 
 func (c BoolCodec) Read(r io.Reader) (bool, error) {
-	panic("unimplemented")
+	in := make([]byte, 1)
+	if _, err := r.Read(in); err != nil {
+		return false, err
+	}
+	if in[0] != 0 && in[0] != 1 {
+		return false, fmt.Errorf("value should be 0 or 1, but was: %d", in[0])
+	}
+	return in[0] == 1, nil
 }
 
 func (c BoolCodec) Write(value bool, w io.Writer) error {
-	panic("unimplemented")
+	var out byte
+	if value {
+		out = 1
+	}
+	_, err := w.Write([]byte{out})
+	return err
 }
