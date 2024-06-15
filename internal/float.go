48c55ee9c5c3662ad489190d24bd52002ac6b93d
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-06-14 18:10:38 -0700

    Add float32 and float64 Codecs.

--- internal/float.go
+++ internal/float.go
@@ -1,23 +1,123 @@
 package internal
 
-import "io"
+import (
+	"encoding/binary"
+	"io"
+	"math"
+)
 
+// This Codec encodes float32s to be consistent with the following
+// ordering. Since the standard does not define their binary formats, no
+// distinction is made between quiet and signaling NaNs.
+//
+//	-NaN
+//	-Infinity
+//	-x, for normal negative numbers x
+//	-s, for subnormal negative numbers s
+//	-0
+//	+0
+//	+s, for subnormal positive numbers s
+//	+x, for normal positive numbers x
+//	+Infinity
+//	+NaN
+//
+// The rest of this comment contains details about IEEE 754 and how this
+// encoding works. Feel free to skip it!
+//
+// The standard binary format of floats is defined by IEEE 754. The
+// represented value is:
+//
+//	+/-1 * mantissa * 2^exponent
+//
+// where the binary format is:
+//
+//	sign - 1 bit
+//	    0 := positive, 1 := negative
+//	exponent - 8 bits
+//	    0x00 :=
+//	        +/-0 if the matissa is zero
+//	        a subnormal number if the mantissa is not zero, the leading
+//	        bit of the mantissa is 0 and is not stored
+//	    0x01-0xFE := normal exponent = value - 127 (-126 to +127)
+//	        the mantissa encodes a value in the range [1.0, 2.0), with
+//	        the leading 1 bit being implicit and not stored
+//	    0xFF :=
+//	        +/- infinity if the mantissa is zero
+//	        NaN if the mantissa is not zero
+//	mantissa - 23 bits
+//	    see above for interpretation
+//
+// Because of the special values, IEEE 754 defines ordering in a way
+// that is not consistent with Codec's semantics. In particular, -0 and
+// +0 are equal and NaN is not comparable to anything, even another NaN.
+// These will all by encoded to some []byte by this Codec, and will be
+// comparable in that representation.
+//
+// By design, a float32's bits interpreted as a signed-magnitude int
+// (not the normal 2's complement int) will result in the right
+// ordering. To convert a float32's bits to the correct []byte
+// lexicographical ordering, we need to:
+//
+//	flip the high bit if the sign bit is 0
+//	flip all the bits if the sign bit is 1
 type Float32Codec struct{}
 
+const highBit32 = 0x80_00_00_00
+const allBits32 = 0xFF_FF_FF_FF
+
 func (c Float32Codec) Read(r io.Reader) (float32, error) {
-	panic("unimplemented")
+	var bits uint32
+	if err := binary.Read(r, binary.BigEndian, &bits); err != nil {
+		return 0.0, err
+	}
+	if bits&highBit32 == 0 {
+		bits ^= allBits32
+	} else {
+		bits ^= highBit32
+	}
+	return math.Float32frombits(bits), nil
 }
 
 func (c Float32Codec) Write(w io.Writer, value float32) error {
-	panic("unimplemented")
+	bits := math.Float32bits(value)
+	if bits&highBit32 == 0 {
+		bits ^= highBit32
+	} else {
+		bits ^= allBits32
+	}
+	return binary.Write(w, binary.BigEndian, bits)
 }
 
+// This has the same general behavior as Float32Codec. The IEEE 754
+// format differs slightly, but is otherwise analagous.
+//
+//	sign - 1 bit
+//	exponent - 11 bits
+//	mantissa - 52 bits
 type Float64Codec struct{}
 
+const highBit64 = 0x80_00_00_00_00_00_00_00
+const allBits64 = 0xFF_FF_FF_FF_FF_FF_FF_FF
+
 func (c Float64Codec) Read(r io.Reader) (float64, error) {
-	panic("unimplemented")
+	var bits uint64
+	if err := binary.Read(r, binary.BigEndian, &bits); err != nil {
+		return 0.0, err
+	}
+	if bits&highBit64 == 0 {
+		bits ^= allBits64
+	} else {
+		bits ^= highBit64
+	}
+	return math.Float64frombits(bits), nil
 }
 
 func (c Float64Codec) Write(w io.Writer, value float64) error {
-	panic("unimplemented")
+	bits := math.Float64bits(value)
+	if bits&highBit64 == 0 {
+		bits ^= highBit64
+	} else {
+		bits ^= allBits64
+	}
+	return binary.Write(w, binary.BigEndian, bits)
 }
--- internal/float_test.go
+++ internal/float_test.go
@@ -2,118 +2,326 @@ package internal
 
 import (
 	"bytes"
-	"io"
+	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFloat32Codec_Read(t *testing.T) {
-	type args struct {
-		r io.Reader
+const (
+	maskSign32 uint32 = 0x80_00_00_00
+	maskExp32  uint32 = 0x7F_80_00_00
+	maskMant32 uint32 = 0x00_7F_FF_FF
+	maskSign64 uint64 = 0x80_00_00_00_00_00_00_00
+	maskExp64  uint64 = 0x7F_F0_00_00_00_00_00_00
+	maskMant64 uint64 = 0x00_0F_FF_FF_FF_FF_FF_FF
+)
+
+func exp32(value float32) uint32 {
+	return maskExp32 & math.Float32bits(value)
+}
+
+func mant32(value float32) uint32 {
+	return maskMant32 & math.Float32bits(value)
+}
+
+func exp64(value float64) uint64 {
+	return maskExp64 & math.Float64bits(value)
+}
+
+func mant64(value float64) uint64 {
+	return maskMant64 & math.Float64bits(value)
+}
+
+var (
+	// All the IEEE 745 32-bit patterns on the boundaries between
+	// different semantics, defined here in the same order that they
+	// should be in when encoded by the Codec. "Min" and "Max" in these
+	// variable names denotes absolute magnitude (distance from 0).
+	negMaxNaN32       float32 = math.Float32frombits(0xFF_FF_FF_FF)
+	negMinNaN32       float32 = math.Float32frombits(0xFF_80_00_01)
+	negInf32          float32 = math.Float32frombits(0xFF_80_00_00)
+	negMaxNormal32    float32 = math.Float32frombits(0xFF_7F_FF_FF)
+	negMinNormal32    float32 = math.Float32frombits(0x80_80_00_00)
+	negMaxSubnormal32 float32 = math.Float32frombits(0x80_7F_FF_FF)
+	negMinSubnormal32 float32 = math.Float32frombits(0x80_00_00_01)
+	negZero32         float32 = math.Float32frombits(0x80_00_00_00)
+	posZero32         float32 = math.Float32frombits(0x00_00_00_00)
+	posMinSubnormal32 float32 = math.Float32frombits(0x00_00_00_01)
+	posMaxSubnormal32 float32 = math.Float32frombits(0x00_7F_FF_FF)
+	posMinNormal32    float32 = math.Float32frombits(0x00_80_00_00)
+	posMaxNormal32    float32 = math.Float32frombits(0x7F_7F_FF_FF)
+	posInf32          float32 = math.Float32frombits(0x7F_80_00_00)
+	posMinNaN32       float32 = math.Float32frombits(0x7F_80_00_01)
+	posMaxNaN32       float32 = math.Float32frombits(0x7F_FF_FF_FF)
+
+	// Same for IEEE 745 64-bit patterns.
+	negMaxNaN64       float64 = math.Float64frombits(0xFF_FF_FF_FF_FF_FF_FF_FF)
+	negMinNaN64       float64 = math.Float64frombits(0xFF_F0_00_00_00_00_00_01)
+	negInf64          float64 = math.Float64frombits(0xFF_F0_00_00_00_00_00_00)
+	negMaxNormal64    float64 = math.Float64frombits(0xFF_EF_FF_FF_FF_FF_FF_FF)
+	negMinNormal64    float64 = math.Float64frombits(0x80_10_00_00_00_00_00_00)
+	negMaxSubnormal64 float64 = math.Float64frombits(0x80_0F_FF_FF_FF_FF_FF_FF)
+	negMinSubnormal64 float64 = math.Float64frombits(0x80_00_00_00_00_00_00_01)
+	negZero64         float64 = math.Float64frombits(0x80_00_00_00_00_00_00_00)
+	posZero64         float64 = math.Float64frombits(0x00_00_00_00_00_00_00_00)
+	posMinSubnormal64 float64 = math.Float64frombits(0x00_00_00_00_00_00_00_01)
+	posMaxSubnormal64 float64 = math.Float64frombits(0x00_0F_FF_FF_FF_FF_FF_FF)
+	posMinNormal64    float64 = math.Float64frombits(0x00_10_00_00_00_00_00_00)
+	posMaxNormal64    float64 = math.Float64frombits(0x7F_EF_FF_FF_FF_FF_FF_FF)
+	posInf64          float64 = math.Float64frombits(0x7F_F0_00_00_00_00_00_00)
+	posMinNaN64       float64 = math.Float64frombits(0x7F_F0_00_00_00_00_00_01)
+	posMaxNaN64       float64 = math.Float64frombits(0x7F_FF_FF_FF_FF_FF_FF_FF)
+)
+
+// Some of these tests are to make sure I didn't fat-finger any of the
+// values above, which I absolutely did the first time around. The
+// others test the semantics of the encoding.
+
+// Asserts the the bits of b are exactly one more than the bits of a.
+func assertNext32(t *testing.T, a, b float32) {
+	assert.Equal(t, math.Float32bits(a)+1, math.Float32bits(b))
+}
+
+// Asserts the the bits of b are exactly one more than the bits of a.
+func assertNext64(t *testing.T, a, b float64) {
+	assert.Equal(t, math.Float64bits(a)+1, math.Float64bits(b))
+}
+
+// Tests the expected ordering of the IEEE 754 32-bit encodings, if
+// their bits are interpreted as a uint32. This ensures that none of the
+// types overlap (negative subnormals and negative normals, e.g.). Other
+// tests will test other semantics.
+func TestIEEEOrdering32(t *testing.T) {
+	assert.IsIncreasing(t, []uint32{
+		math.Float32bits(posZero32),
+		math.Float32bits(posMinSubnormal32),
+		math.Float32bits(posMaxSubnormal32),
+		math.Float32bits(posMinNormal32),
+		math.Float32bits(posMaxNormal32),
+		math.Float32bits(posInf32),
+		math.Float32bits(posMinNaN32),
+		math.Float32bits(posMaxNaN32),
+		math.Float32bits(negZero32),
+		math.Float32bits(negMinSubnormal32),
+		math.Float32bits(negMaxSubnormal32),
+		math.Float32bits(negMinNormal32),
+		math.Float32bits(negMaxNormal32),
+		math.Float32bits(negInf32),
+		math.Float32bits(negMinNaN32),
+		math.Float32bits(negMaxNaN32),
+	})
+
+	assert.Equal(t, uint32(0), math.Float32bits(posZero32))
+	assert.Equal(t, uint32(math.MaxUint32), math.Float32bits(negMaxNaN32))
+
+	assertNext32(t, posZero32, posMinSubnormal32)
+	assertNext32(t, posMaxSubnormal32, posMinNormal32)
+	assertNext32(t, posMaxNormal32, posInf32)
+	assertNext32(t, posInf32, posMinNaN32)
+	assertNext32(t, posMaxNaN32, negZero32)
+	assertNext32(t, negZero32, negMinSubnormal32)
+	assertNext32(t, negMaxSubnormal32, negMinNormal32)
+	assertNext32(t, negMaxNormal32, negInf32)
+	assertNext32(t, negInf32, negMinNaN32)
+}
+
+// Test semantic ordering for orderable values (not the NaNs). This also
+// tests thoat all the normal/subnormal constants are neither NaN nor
+// infinite.
+func TestSemanticOrdering32(t *testing.T) {
+	assert.IsIncreasing(t, []float32{
+		negInf32,
+		negMaxNormal32,
+		negMinNormal32,
+		negMaxSubnormal32,
+		negMinSubnormal32,
+		posZero32,
+		posMinSubnormal32,
+		posMaxSubnormal32,
+		posMinNormal32,
+		posMaxNormal32,
+		posInf32,
+	})
+}
+
+// Tests that the 32-bit constants are what their names say they are.
+// Other tests will test other semantics.
+func TestNames32(t *testing.T) {
+	// Testable exact values
+	assert.Equal(t, math.Inf(-1), float64(negInf32), "-Inf: %x", negInf32)
+	assert.Equal(t, math.Inf(1), float64(posInf32), "+Inf: %x", posInf32)
+	assert.Equal(t, -float32(math.MaxFloat32), negMaxNormal32, "max negative float32: %x", negMaxNormal32)
+	assert.Equal(t, float32(math.MaxFloat32), posMaxNormal32, "max positive float32: %x", posMaxNormal32)
+	assert.Equal(t, -float32(math.SmallestNonzeroFloat32), negMinSubnormal32, "min negative float32: %x", negMinSubnormal32)
+	assert.Equal(t, float32(math.SmallestNonzeroFloat32), posMinSubnormal32, "min positive float32: %x", posMinSubnormal32)
+	assert.Equal(t, float32(math.Copysign(0.0, -1.0)), negZero32, "should be -0.0: %x", negZero32)
+	assert.Equal(t, float32(math.Copysign(0.0, 1.0)), posZero32, "should be +0.0: %x", posZero32)
+
+	// Test NaN, exponents, and matissas
+	for _, x := range []float32{negMaxNaN32, negMinNaN32, posMinNaN32, posMaxNaN32} {
+		assert.True(t, math.IsNaN(float64(x)), "should be NaN: %x", x)
 	}
-	tests := []struct {
-		name    string
-		c       Float32Codec
-		args    args
-		want    float32
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+	for _, x := range []float32{negMaxNormal32, negMinNormal32, posMinNormal32, posMaxNormal32} {
+		assert.NotEqual(t, uint32(0), exp32(x), "non-zero normal numbers should have a non-zero exponent: %x", x)
+		assert.NotEqual(t, maskExp32, exp32(x), "non-zero normal numbers should have a non-0xFF exponent: %x", x)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := Float32Codec{}
-			got, err := c.Read(tt.args.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Float32Codec.Read() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("Float32Codec.Read() = %v, want %v", got, tt.want)
-			}
-		})
+	for _, x := range []float32{negMaxSubnormal32, negMinSubnormal32, posMinSubnormal32, posMaxSubnormal32} {
+		assert.Equal(t, uint32(0), exp32(x), "subnormal numbers should have a zero exponent: %x", x)
+		assert.NotEqual(t, uint32(0), mant32(x), "subnormal numbers should have a non-zero mantissa: %x", x)
 	}
 }
 
-func TestFloat32Codec_Write(t *testing.T) {
-	type args struct {
-		value float32
+// Test that the encoded forms have the right ordering. Note that unlike
+// the floats
+func TestFloat32CodecOrdering(t *testing.T) {
+	encode := func(value float32) []byte {
+		c := Float32Codec{}
+		var b bytes.Buffer
+		if err := c.Write(&b, value); err != nil {
+			panic(err)
+		}
+		return b.Bytes()
 	}
-	tests := []struct {
-		name    string
-		c       Float32Codec
-		args    args
-		wantW   string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+
+	assert.Equal(t, []byte{0x00, 0x00, 0x00, 0x00}, encode(negMaxNaN32))
+	assert.Equal(t, []byte{0xFF, 0xFF, 0xFF, 0xFF}, encode(posMaxNaN32))
+
+	assert.IsIncreasing(t, [][]byte{
+		encode(negMaxNaN32),
+		encode(negMinNaN32),
+		encode(negInf32),
+		encode(negMaxNormal32),
+		encode(negMinNormal32),
+		encode(negMaxSubnormal32),
+		encode(negMinSubnormal32),
+		encode(negZero32),
+		encode(posZero32),
+		encode(posMinSubnormal32),
+		encode(posMaxSubnormal32),
+		encode(posMinNormal32),
+		encode(posMaxNormal32),
+		encode(posInf32),
+		encode(posMinNaN32),
+		encode(posMaxNaN32),
+	})
+}
+
+// Tests the expected ordering of the IEEE 754 64-bit encodings, if
+// their bits are interpreted as a uint64. This ensures that none of the
+// types overlap (negative subnormals and negative normals, e.g.). Other
+// tests will test other semantics.
+func TestIEEEOrdering64(t *testing.T) {
+	assert.IsIncreasing(t, []uint64{
+		math.Float64bits(posZero64),
+		math.Float64bits(posMinSubnormal64),
+		math.Float64bits(posMaxSubnormal64),
+		math.Float64bits(posMinNormal64),
+		math.Float64bits(posMaxNormal64),
+		math.Float64bits(posInf64),
+		math.Float64bits(posMinNaN64),
+		math.Float64bits(posMaxNaN64),
+		math.Float64bits(negZero64),
+		math.Float64bits(negMinSubnormal64),
+		math.Float64bits(negMaxSubnormal64),
+		math.Float64bits(negMinNormal64),
+		math.Float64bits(negMaxNormal64),
+		math.Float64bits(negInf64),
+		math.Float64bits(negMinNaN64),
+		math.Float64bits(negMaxNaN64),
+	})
+
+	assert.Equal(t, uint64(0), math.Float64bits(posZero64))
+	assert.Equal(t, uint64(math.MaxUint64), math.Float64bits(negMaxNaN64))
+
+	assertNext64(t, posZero64, posMinSubnormal64)
+	assertNext64(t, posMaxSubnormal64, posMinNormal64)
+	assertNext64(t, posMaxNormal64, posInf64)
+	assertNext64(t, posInf64, posMinNaN64)
+	assertNext64(t, posMaxNaN64, negZero64)
+	assertNext64(t, negZero64, negMinSubnormal64)
+	assertNext64(t, negMaxSubnormal64, negMinNormal64)
+	assertNext64(t, negMaxNormal64, negInf64)
+	assertNext64(t, negInf64, negMinNaN64)
+}
+
+// Test semantic ordering for orderable values (not the NaNs). This also
+// tests thoat all the normal/subnormal constants are neither NaN nor
+// infinite.
+func TestSemanticOrdering64(t *testing.T) {
+	assert.IsIncreasing(t, []float64{
+		negInf64,
+		negMaxNormal64,
+		negMinNormal64,
+		negMaxSubnormal64,
+		negMinSubnormal64,
+		posZero64,
+		posMinSubnormal64,
+		posMaxSubnormal64,
+		posMinNormal64,
+		posMaxNormal64,
+		posInf64,
+	})
+}
+
+// Tests that the 64-bit constants are what their names say they are.
+// Other tests will test other semantics.
+func TestNames64(t *testing.T) {
+	// Testable exact values
+	assert.Equal(t, math.Inf(-1), negInf64, "-Inf: %x", negInf64)
+	assert.Equal(t, math.Inf(1), posInf64, "+Inf: %x", posInf64)
+	assert.Equal(t, -math.MaxFloat64, negMaxNormal64, "max negative float64: %x", negMaxNormal64)
+	assert.Equal(t, math.MaxFloat64, posMaxNormal64, "max positive float64: %x", posMaxNormal64)
+	assert.Equal(t, -math.SmallestNonzeroFloat64, negMinSubnormal64, "min negative float64: %x", negMinSubnormal64)
+	assert.Equal(t, math.SmallestNonzeroFloat64, posMinSubnormal64, "min positive float64: %x", posMinSubnormal64)
+	assert.Equal(t, math.Copysign(0.0, -1.0), negZero64, "should be -0.0: %x", negZero64)
+	assert.Equal(t, math.Copysign(0.0, 1.0), posZero64, "should be +0.0: %x", posZero64)
+
+	// Test NaN, exponents, and matissas
+	for _, x := range []float64{negMaxNaN64, negMinNaN64, posMinNaN64, posMaxNaN64} {
+		assert.True(t, math.IsNaN(x), "should be NaN: %x", x)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := Float32Codec{}
-			w := &bytes.Buffer{}
-			if err := c.Write(w, tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("Float32Codec.Write() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotW := w.String(); gotW != tt.wantW {
-				t.Errorf("Float32Codec.Write() = %v, want %v", gotW, tt.wantW)
-			}
-		})
+	for _, x := range []float64{negMaxNormal64, negMinNormal64, posMinNormal64, posMaxNormal64} {
+		assert.NotEqual(t, uint64(0), exp64(x), "non-zero normal numbers should have a non-zero exponent: %x", x)
+		assert.NotEqual(t, maskExp64, exp64(x), "non-zero normal numbers should have a non-0xFF exponent: %x", x)
+	}
+	for _, x := range []float64{negMaxSubnormal64, negMinSubnormal64, posMinSubnormal64, posMaxSubnormal64} {
+		assert.Equal(t, uint64(0), exp64(x), "subnormal numbers should have a zero exponent: %x", x)
+		assert.NotEqual(t, uint64(0), mant64(x), "subnormal numbers should have a non-zero mantissa: %x", x)
 	}
 }
 
-func TestFloat64Codec_Read(t *testing.T) {
-	type args struct {
-		r io.Reader
+// Test that the encoded forms have the right ordering. Note that unlike
+// the floats
+func TestFloat64CodecOrdering(t *testing.T) {
+	encode := func(value float64) []byte {
+		c := Float64Codec{}
+		var b bytes.Buffer
+		if err := c.Write(&b, value); err != nil {
+			panic(err)
+		}
+		return b.Bytes()
 	}
-	tests := []struct {
-		name    string
-		c       Float64Codec
-		args    args
-		want    float64
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := Float64Codec{}
-			got, err := c.Read(tt.args.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Float64Codec.Read() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("Float64Codec.Read() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestFloat64Codec_Write(t *testing.T) {
-	type args struct {
-		value float64
-	}
-	tests := []struct {
-		name    string
-		c       Float64Codec
-		args    args
-		wantW   string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := Float64Codec{}
-			w := &bytes.Buffer{}
-			if err := c.Write(w, tt.args.value); (err != nil) != tt.wantErr {
-				t.Errorf("Float64Codec.Write() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if gotW := w.String(); gotW != tt.wantW {
-				t.Errorf("Float64Codec.Write() = %v, want %v", gotW, tt.wantW)
-			}
-		})
-	}
+	assert.Equal(t, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, encode(negMaxNaN64))
+	assert.Equal(t, []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}, encode(posMaxNaN64))
+
+	assert.IsIncreasing(t, [][]byte{
+		encode(negMaxNaN64),
+		encode(negMinNaN64),
+		encode(negInf64),
+		encode(negMaxNormal64),
+		encode(negMinNormal64),
+		encode(negMaxSubnormal64),
+		encode(negMinSubnormal64),
+		encode(negZero64),
+		encode(posZero64),
+		encode(posMinSubnormal64),
+		encode(posMaxSubnormal64),
+		encode(posMinNormal64),
+		encode(posMaxNormal64),
+		encode(posInf64),
+		encode(posMinNaN64),
+		encode(posMaxNaN64),
+	})
 }
