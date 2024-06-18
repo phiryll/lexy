9573478b74e22e124799a92f55caf3a1e51e02fd
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-06-18 15:05:29 -0700

    Use semantic line breaks in escape.go.
    ref #6

--- internal/escape.go
+++ internal/escape.go
@@ -1,20 +1,17 @@
 package internal
 
-// Functions for delimiting elements of aggregates (maps, slices, and
-// structs) and escaping. These are defined in away that preserves the
-// lexicographical []byte ordering of encoded aggregate types. For
-// example, the encoding of ["ab", "cde"] needs to less than the
-// encoding of ["abc", "de"], because "ab" is less than "abc". The
-// delimiter can't itself be used to escape a delimiter because it leads
-// to ambiguities, so there needs to be a distinct escape character.
+// Functions for delimiting elements of aggregates (maps, slices, and structs) and escaping.
+// The lexicographical binary ordering of encoded aggregates is preserved.
+// For example, ["ab", "cde"] is less than ["aba", "de"], because "ab" is less than "aba".
+// The delimiter can't itself be used to escape a delimiter because it leads to ambiguities,
+// so there needs to be a distinct escape character.
 
-// This comment explains why 0x00 and 0x01 were chosen for the delimiter
-// and escape values. Strings are used in the following examples for
-// clarity, with "," and "\" denoting the delimiter and escape bytes.
-// The form of the examples is (input []string -> encoded string), with
-// all input characters having their natural meaning (no delimiters or
-// escapes). The encodings for maps and structs will be analogous.
+// This comment explains why the delimiter and escape values must be 0x00 and 0x01.
+// Strings are used for clarity, with "," and "\" denoting the delimiter and escape bytes.
+// All input characters have their natural meaning (no delimiters or escapes).
+// The encodings for maps and structs will be analogous.
 //
+//      input slice  -> encoded string
 //   A: ["a", "bc"]  -> a,bc
 //   B: ["a", ",bc"] -> a,\,bc
 //   C: ["a", "\bc"] -> a,\\bc
@@ -25,21 +22,17 @@ package internal
 // B and E are an example of why the delimiter can't be its own escape,
 // the encoded strings would both be "a,,,b".
 //
-// A, B, and C must all be less than D, E, and F. Therefore "," must be
-// less than all other values, including the escape. The delimiter must
-// be 0x00.
+// A, B, and C must all be less than D, E, and F.
+// We can see "," must be less than all other values including the escape, so it must be 0x00.
 //
-// Since the delimiter is less than all other values, E must be less
-// than D (first slice element "a," < "ab"), so the encoded value
-// "a\,,bc" must be less than "ab,c". Therefore "\" must be less than
-// all other values except the delimiter. The escape must be 0x01.
+// Since "," is less than everything else, E < D (first slice element "a," < "ab"). Therefore "a\,,bc" < "ab,c".
+// We can see "/" must be less than all other values except the delimiter, so it must be 0x01.
 
-// Used to delimit elements of an aggregate - slice elements, map keys
-// and values, and struct field names and values.
+// delimiter is used to delimit elements of an aggregate value.
 const delimiter byte = 0x00
 
-// Used to escape the delimiter and escape bytes when they appear in
-// data. This includes appearing in the encodings of nested aggregates,
-// because those are still just data at the level of the enclosing
-// aggregate.
+// escape is used the escape the delimiter and escape bytes when they appear in data.
+//
+// This includes appearing in the encodings of nested aggregates,
+// because those are still just data at the level of the enclosing aggregate.
 const escape byte = 0x01
