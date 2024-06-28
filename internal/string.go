ab3c7d403e4eab797a0913136cdf5acc5ca7e542
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-06-27 19:31:05 -0700

    Add prefixes to string codec, WIP.
    Ref #31

--- internal/string.go
+++ internal/string.go
@@ -1,16 +1,17 @@
 package internal
 
 import (
+	"fmt"
 	"io"
 	"strings"
 )
 
 // StringCodec is the Codec for strings.
 //
-// A string is encoded as its bytes. Nothing is written for an empty string.
+// A string is encoded as its bytes following PrefixZeroValue or PrefixNonZeroValue.
 // Read will fully consume its argument io.Reader.
 // If a string is part of a larger aggregate and not fixed-length,
-// it should be escaped and delimiter-terminated by the enclosing Codec.
+// its encoding should be escaped and delimiter-terminated by the enclosing Codec.
 //
 // The order of strings, and this encoding, may be surprising.
 // A string in go is essentially an immutable []byte without text semantics.
@@ -20,6 +21,27 @@ import (
 type StringCodec struct{}
 
 func (c StringCodec) Read(r io.Reader) (string, error) {
+	// TODO: clean this up a little bit, it's messy
+	prefix := []byte{0}
+	n, err := r.Read(prefix)
+	if n == 0 {
+		if err == nil {
+			return "", fmt.Errorf("no bytes read and no error")
+		}
+		return "", err
+	}
+	if prefix[0] == PrefixZeroValue {
+		if err == io.EOF {
+			return "", nil
+		}
+		return "", err
+	}
+	if err != nil {
+		return "", err
+	}
+	if prefix[0] != PrefixNonZeroValue {
+		return "", fmt.Errorf("unexpected prefix")
+	}
 	var buf strings.Builder
 	if _, err := io.Copy(&buf, r); err != nil {
 		return "", err
@@ -28,6 +50,13 @@ func (c StringCodec) Read(r io.Reader) (string, error) {
 }
 
 func (c StringCodec) Write(w io.Writer, value string) error {
+	if value == "" {
+		_, err := w.Write(prefixZero)
+		return err
+	}
+	if _, err := w.Write(prefixNonZero); err != nil {
+		return err
+	}
 	_, err := io.WriteString(w, value)
 	return err
 }
--- internal/string_test.go
+++ internal/string_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestStringCodec(t *testing.T) {
 	testCodec[string](t, internal.StringCodec{}, []testCase[string]{
-		{"empty", "", []byte(nil)},
-		{"a", "a", []byte("a")},
-		{"xyz", "xyz", []byte("xyz")},
-		{"⌘", "⌘", []byte{0xE2, 0x8C, 0x98}},
+		{"empty", "", []byte{0x03}},
+		{"a", "a", []byte{0x04, 'a'}},
+		{"xyz", "xyz", []byte{0x04, 'x', 'y', 'z'}},
+		{"⌘", "⌘", []byte{0x04, 0xE2, 0x8C, 0x98}},
 	})
 }
