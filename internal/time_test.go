92bf454ea339b86e5bc3374c4069b4cb3cb21d96
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-07-05 11:24:17 -0700

    Add Berlin timezone to tests, need at least one East of UTC.

--- internal/time_test.go
+++ internal/time_test.go
@@ -13,7 +13,11 @@ import (
 func TestTime(t *testing.T) {
 	codec := internal.TimeCodec
 
+	// West of UTC, negative timezone offset
 	locNYC, err := time.LoadLocation("America/New_York")
+	require.NoError(t, err)
+	// East of UTC, positive timezone offset
+	locBerlin, err := time.LoadLocation("Europe/Berlin")
 	require.NoError(t, err)
 	var zero time.Time
 	// Before the epoch start on Jan 1, 1970
@@ -21,6 +25,7 @@ func TestTime(t *testing.T) {
 	local := time.Date(2000, 1, 2, 3, 4, 5, 6, time.Local)
 	utc := time.Date(2000, 1, 2, 3, 4, 5, 600_000_000, time.UTC)
 	nyc := time.Date(2000, 1, 2, 3, 4, 5, 999_999_999, locNYC)
+	berlin := time.Date(2000, 1, 2, 3, 4, 5, 999_999_999, locBerlin)
 	noZoneName, err := time.Parse(time.RFC3339Nano, "2000-01-02T03:04:05.6-05:00")
 	require.NoError(t, err)
 
@@ -33,6 +38,7 @@ func TestTime(t *testing.T) {
 		{"utc", utc},
 		{"local", local},
 		{"nyc", nyc},
+		{"berlin", berlin},
 		{"no zone name", noZoneName},
 	} {
 		t.Run(tt.string, func(t *testing.T) {
@@ -59,11 +65,14 @@ func TestTime(t *testing.T) {
 func TestTimeOrder(t *testing.T) {
 	codec := internal.TimeCodec
 
-	// in order from west to east, expected sort order
+	// in order from west to east, expected sort order,
+	// UTC is between NYC and Berlin.
 	locFixed := time.FixedZone("fixed", -12*3600)
 	locLA, err := time.LoadLocation("America/Los_Angeles")
 	require.NoError(t, err)
 	locNYC, err := time.LoadLocation("America/New_York")
+	require.NoError(t, err)
+	locBerlin, err := time.LoadLocation("Europe/Berlin")
 	require.NoError(t, err)
 
 	// UTC times in order, pos/neg relative to epoch and 6/7 nanoseconds
@@ -87,21 +96,25 @@ func TestTimeOrder(t *testing.T) {
 		{"neg LA 6", negUTC6.In(locLA)},
 		{"neg NYC 6", negUTC6.In(locNYC)},
 		{"neg UTC 6", negUTC6},
+		{"neg Berlin 6", negUTC6.In(locBerlin)},
 
 		{"neg fixed 7", negUTC7.In(locFixed)},
 		{"neg LA 7", negUTC7.In(locLA)},
 		{"neg NYC 7", negUTC7.In(locNYC)},
 		{"neg UTC 7", negUTC7},
+		{"neg Berlin 7", negUTC7.In(locBerlin)},
 
 		{"pos fixed 6", posUTC6.In(locFixed)},
 		{"pos LA 6", posUTC6.In(locLA)},
 		{"pos NYC 6", posUTC6.In(locNYC)},
 		{"pos UTC 6", posUTC6},
+		{"pos Berlin 6", posUTC6.In(locBerlin)},
 
 		{"pos fixed 7", posUTC7.In(locFixed)},
 		{"pos LA 7", posUTC7.In(locLA)},
 		{"pos NYC 7", posUTC7.In(locNYC)},
 		{"pos UTC 7", posUTC7},
+		{"pos Berlin 7", posUTC7.In(locBerlin)},
 	} {
 		t.Run(tt.string, func(t *testing.T) {
 			var b bytes.Buffer
