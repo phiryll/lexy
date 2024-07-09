43c62d7b5b2f06b6a76cf8e1ebc7c3727742126a
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-07-08 19:18:19 -0700

    Add Codec for big.Int values.
    Fixes #3

--- internal/big.go
+++ internal/big.go
@@ -10,14 +10,75 @@ var (
 	BigFloatCodec Codec[*big.Float] = bigFloatCodec{}
 )
 
+// bigIntCodec is the Codec for big.Int values.
+//
+// Values are encoded using this logic:
+//
+//	b := value.Bytes() // absolute value as a big-endian byte slice
+//	size := len(b)
+//	if value < 0 {
+//		write -size using Int64Codec
+//		write b with all bits flipped
+//	} else {
+//		write +size using Int64Codec
+//		write b
+//	}
+//
+// This makes size (negative for negative values) the primary sort key,
+// and the big-endian bytes for the value (bits flipped for negative values) the secondary sort key.
+// The effect is that longer numbers will be ordered closer to +/-infinity.
+// This works because bigInt.Bytes() will never have a leading zero byte.
 type bigIntCodec struct{}
 
 func (c bigIntCodec) Read(r io.Reader) (*big.Int, error) {
-	panic("unimplemented")
+	neg := false
+	size, err := Int64Codec.Read(r)
+	if err != nil {
+		return nil, err
+	}
+	if size < 0 {
+		neg = true
+		size = -size
+	}
+	b := make([]byte, size)
+	n, err := r.Read(b)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	if err == io.EOF {
+		if int64(n) < size {
+			return nil, io.ErrUnexpectedEOF
+		}
+		err = nil
+	}
+	if neg {
+		for i := range b {
+			b[i] ^= 0xFF
+		}
+	}
+	var value big.Int
+	value.SetBytes(b)
+	if neg {
+		value.Neg(&value)
+	}
+	return &value, nil
 }
 
 func (c bigIntCodec) Write(w io.Writer, value *big.Int) error {
-	panic("unimplemented")
+	sign := value.Sign()
+	b := value.Bytes()
+	size := len(b)
+	if sign < 0 {
+		size = -size
+		for i := range b {
+			b[i] ^= 0xFF
+		}
+	}
+	if err := Int64Codec.Write(w, int64(size)); err != nil {
+		return err
+	}
+	_, err := w.Write(b)
+	return err
 }
 
 type bigFloatCodec struct{}
--- internal/big_test.go
+++ internal/big_test.go
@@ -1,9 +1,90 @@
 package internal_test
 
-import "testing"
+import (
+	"bytes"
+	"math/big"
+	"testing"
+
+	"github.com/phiryll/lexy/internal"
+	"github.com/stretchr/testify/assert"
+)
+
+func encodeSize(size int64) []byte {
+	var buf bytes.Buffer
+	internal.Int64Codec.Write(&buf, size)
+	return buf.Bytes()
+}
+
+func newBigInt(s string) *big.Int {
+	var value big.Int
+	value.SetString(s, 10)
+	return &value
+}
+
+func encodeBigInt(s string) []byte {
+	value := newBigInt(s)
+	var b bytes.Buffer
+	if err := internal.BigIntCodec.Write(&b, value); err != nil {
+		panic(err)
+	}
+	return b.Bytes()
+}
 
 func TestBigInt(t *testing.T) {
+	codec := internal.BigIntCodec
+	testCodec[*big.Int](t, codec, []testCase[*big.Int]{
+		{"-257", big.NewInt(-257), append(encodeSize(-2),
+			[]byte{0xFE, 0xFE}...)},
+		{"-256", big.NewInt(-256), append(encodeSize(-2),
+			[]byte{0xFE, 0xFF}...)},
+		{"-255", big.NewInt(-255), append(encodeSize(-1),
+			[]byte{0x00}...)},
+		{"-254", big.NewInt(-254), append(encodeSize(-1),
+			[]byte{0x01}...)},
+		{"-2", big.NewInt(-2), append(encodeSize(-1),
+			[]byte{0xFD}...)},
+		{"-1", big.NewInt(-1), append(encodeSize(-1),
+			[]byte{0xFE}...)},
+		{"0", big.NewInt(0), append(encodeSize(0),
+			[]byte{}...)},
+		{"+1", big.NewInt(1), append(encodeSize(1),
+			[]byte{0x01}...)},
+		{"+2", big.NewInt(2), append(encodeSize(1),
+			[]byte{0x02}...)},
+		{"254", big.NewInt(254), append(encodeSize(1),
+			[]byte{0xFE}...)},
+		{"255", big.NewInt(255), append(encodeSize(1),
+			[]byte{0xFF}...)},
+		{"256", big.NewInt(256), append(encodeSize(2),
+			[]byte{0x01, 0x00}...)},
+		{"257", big.NewInt(257), append(encodeSize(2),
+			[]byte{0x01, 0x01}...)},
+	})
 
+	testCodecRoundTrip[*big.Int](t, codec, []testCase[*big.Int]{
+		{"big positive", newBigInt("1234567890123456789012345678901234567890"), nil},
+		{"big negative", newBigInt("-1234567890123456789012345678901234567890"), nil},
+	})
+}
+
+func TestBigIntOrdering(t *testing.T) {
+	assert.IsIncreasing(t, [][]byte{
+		encodeBigInt("-12345"),
+		encodeBigInt("-12344"),
+		encodeBigInt("-12343"),
+		encodeBigInt("-257"),
+		encodeBigInt("-256"),
+		encodeBigInt("-255"),
+		encodeBigInt("-1"),
+		encodeBigInt("0"),
+		encodeBigInt("1"),
+		encodeBigInt("255"),
+		encodeBigInt("256"),
+		encodeBigInt("257"),
+		encodeBigInt("12343"),
+		encodeBigInt("12344"),
+		encodeBigInt("12345"),
+	})
 }
 
 func TestBigFloat(t *testing.T) {
