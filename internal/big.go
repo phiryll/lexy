2b1c315944e55f2f32580ba07c095e21752235e5
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-07-11 15:56:46 -0700

    Add big.Float Codec.
    Fixes #4

--- internal/big.go
+++ internal/big.go
@@ -77,12 +77,222 @@ func (c bigIntCodec) Write(w io.Writer, value *big.Int) error {
 	return err
 }
 
+// bigFloatCodec is the Codec for big.Float values.
+//
+// This is roughly similar to the float32/64 Codecs, but there are some wrinkles.
+// There is no good way to get the mantissa in a binary form,
+// the standard library just doesn't expose that information.
+// A description of the encoding and why it does what it does follows.
+//
+// Shift a copy of the big.Float so that:
+//
+//	all significant bits are to the left of the point,
+//	the high bit of the high byte is 1, and
+//	the low byte is not 0
+//
+// Get the big.Int value of the shifted big.Float.
+// This is the mantissa if you interpret it as being immediately to the right of the point,
+// which is the standard for representing a mantissa, 0.5 <= mantissa < 1.0.
+// The only purpose of this is to get the exact []byte of the mantissa
+// out of a big.Float without resorting to parsing.
+// None of this will change the exponent or precision that is encoded.
+//
+// For example (binary, non-significant bits are shown as "-", assume they're all 0):
+//
+//	A = 7.0 (prec 3)  = 0.111- ----           * 2**3
+//	B = 7.0 (prec 4)  = 0.1110 ----           * 2**3
+//	C = 7.0 (prec 10) = 0.1110 0000 00-- ---- * 2**3
+//
+// All of these are the same semantic value, but with different precisions.
+// After the shift we have (precision does not change)
+//
+//	A shift by 5  = 0.111- ----           * 2**8
+//		prec = 3, prec - exp = 0
+//	    Int mant  = 111- ---- = 224
+//	B shift by 5  = 0.1110 ----           * 2**8
+//		prec = 4, prec - exp = 1
+//	    Int mant  = 1110 ---- = 224
+//	C shift by 13 = 0.1110 0000 00-- ---- * 2**16
+//		prec = 10, prec - exp = 7
+//	    Int mant  = 1110 0000 00-- ---- = 57344
+//
+// Since the mantissa is variable length, it must be escaped and delimiter-terminated.
+// The precision and rounding mode must be encoded following
+// the sign, exponent, and mantissa to keep the lexicographical ordering correct.
+//
+// We can see C > A and C > B since that's what the necessary encoding does.
+// Therefore, B > A if the ordering is consistent with C > A and C > B, higher precisions are greater.
+// For negative values, higher precisions are lesser.
+// This leads to encoding the precision immediately after the mantissa.
+//
+// Encode:
+//
+//	write prefix int8: -3/-2/-1/+1/+2/+3 for
+//		-Inf / (-Inf,0) / -0 / +0 / (0,+Inf) / +Inf
+//	if infinite or zero, we're done
+//	write int32 exponent
+//		negate exponent first if Float is negative
+//	write the (big-endian) bytes of the big.Int of the shifted mantissa
+//		do *not* encode with bitIntCodec, write the raw bytes
+//		trailing non-sigificant bits will already be zero, the conversion to big.Int requires it
+//		escape and delimiter terminate, then flip bits (including the terminator) if Float is negative
+//	write int32 precision
+//		negate precision first if Float is negative
+//	write uint8 rounding mode
 type bigFloatCodec struct{}
 
+const (
+	negInf       int8 = -3
+	negFinite    int8 = -2
+	negZero      int8 = -1
+	posZero      int8 = +1
+	nonNegFinite int8 = +2
+	posInf       int8 = +3
+)
+
+var modeCodec = uintCodec[big.RoundingMode]{}
+
+func computeShift(exp int32, prec int32) int {
+	// (prec - exp) is a shift of significant bits to immediately left of the point.
+	shift := prec - exp
+	// Shift a little further so the high bit in the high byte is 1.
+	// Equivalently, the exponent is a multiple of 8.
+	// There are exactly prec bits to that leading bit,
+	// so shift enough to round up prec to the nearest multiple of 8.
+	adjustment := (-prec) % 8
+	if adjustment < 0 {
+		adjustment += 8
+	}
+	return int(shift + adjustment)
+}
+
 func (c bigFloatCodec) Read(r io.Reader) (*big.Float, error) {
-	panic("unimplemented")
+	prefix, err := Int8Codec.Read(r)
+	if err != nil {
+		return nil, err
+	}
+	signbit := prefix < 0
+	if prefix == negInf || prefix == posInf {
+		var value big.Float
+		return value.SetInf(signbit), nil
+	}
+	if prefix == negZero || prefix == posZero {
+		var value big.Float
+		if signbit {
+			value.Neg(&value)
+		}
+		return &value, nil
+	}
+	mantReader := r
+	if signbit {
+		mantReader = inverseReader{r}
+	}
+
+	exp, err := Int32Codec.Read(r)
+	if err != nil {
+		return nil, err
+	}
+	mantBytes, err := Unescape(mantReader)
+	if err != nil {
+		return nil, err
+	}
+	prec, err := Int32Codec.Read(r)
+	if err != nil {
+		return nil, err
+	}
+	mode, err := modeCodec.Read(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if signbit {
+		exp = -exp
+		prec = -prec
+	}
+	shift := computeShift(exp, prec)
+
+	var mantInt big.Int
+	var value big.Float
+	mantInt.SetBytes(mantBytes)
+	value.SetInt(&mantInt)
+	value.SetMantExp(&value, -shift)
+	value.SetPrec(uint(prec))
+	value.SetMode(mode)
+	if signbit {
+		value.Neg(&value)
+	}
+	return &value, nil
 }
 
 func (c bigFloatCodec) Write(w io.Writer, value *big.Float) error {
-	panic("unimplemented")
+	// exp and prec are int and uint, but internally they're 32 bits
+	// use a signed prec here because we're doing possibly negative calculations with it
+	signbit := value.Signbit() // true if negative or negative zero
+	exp := int32(value.MantExp(nil))
+	prec := int32(value.Prec())
+	mode := value.Mode() // uint8
+	shift := computeShift(exp, prec)
+
+	isInf := value.IsInf()
+	isZero := prec == 0
+
+	var prefix int8
+	switch {
+	case isInf && signbit:
+		prefix = negInf
+	case isInf && !signbit:
+		prefix = posInf
+	case isZero && signbit:
+		prefix = negZero
+	case isZero && !signbit:
+		prefix = posZero
+	case signbit:
+		prefix = negFinite
+	case !signbit:
+		prefix = nonNegFinite
+	}
+	if err := Int8Codec.Write(w, prefix); err != nil {
+		return err
+	}
+	if isInf || isZero {
+		return nil
+	}
+
+	mantWriter := w
+	if signbit {
+		// These values are no longer being used except to write them
+		exp = -exp
+		prec = -prec
+		mantWriter = inverseWriter{w}
+	}
+
+	if err := Int32Codec.Write(w, exp); err != nil {
+		return err
+	}
+
+	var copy big.Float
+	copy.Copy(value)
+	copy.SetMantExp(&copy, shift)
+	mantInt, acc := copy.Int(nil)
+	if acc != big.Exact {
+		panic("unexpected failure while encoding big.Float")
+	}
+	mantBytes := mantInt.Bytes()
+	if signbit {
+		invertSlice(mantBytes)
+		for i := range mantBytes {
+			mantBytes[i] ^= 0xFF
+		}
+	}
+	if _, err := Escape(mantWriter, mantBytes); err != nil {
+		return err
+	}
+	if _, err := mantWriter.Write(del); err != nil {
+		return err
+	}
+
+	if err := Int32Codec.Write(w, prec); err != nil {
+		return err
+	}
+	return modeCodec.Write(w, mode)
 }
--- internal/big_test.go
+++ internal/big_test.go
@@ -87,6 +87,156 @@ func TestBigIntOrdering(t *testing.T) {
 	})
 }
 
-func TestBigFloat(t *testing.T) {
+func newBigFloat(f float64, shift int, prec uint) *big.Float {
+	value := big.NewFloat(f)
+	value.SetPrec(prec)
+	value.SetMantExp(value, shift)
+	return value
+}
 
+func encodeBigFloat(value *big.Float) []byte {
+	var b bytes.Buffer
+	if err := internal.BigFloatCodec.Write(&b, value); err != nil {
+		panic(err)
+	}
+	return b.Bytes()
+}
+
+func TestBigFloat(t *testing.T) {
+	var negInf, posInf, negZero, posZero big.Float
+	negInf.SetInf(true)
+	posInf.SetInf(false)
+	negZero.Neg(&negZero)
+	assert.True(t, negZero.Signbit())
+	assert.False(t, posZero.Signbit())
+	assert.Equal(t, 0, negZero.Cmp(&posZero))
+	assert.NotEqual(t, &negZero, &posZero)
+
+	var complexWhole big.Float
+	// Parse truncates to 64 bits if precision is currently 0
+	complexWhole.SetPrec(100000)
+	complexWhole.Parse("12345678901234567890123456789012345678901234567890", 10)
+	complexWhole.SetPrec(complexWhole.MinPrec())
+
+	var complexMixed big.Float
+	// Parse truncates to 64 bits if precision is currently 0
+	complexMixed.SetPrec(100000)
+	complexMixed.Parse("12345678901234567890123456789012345678901234567890"+
+		".12345678901234567890123456789012345678901234567890", 10)
+	complexMixed.SetPrec(complexMixed.MinPrec())
+
+	var complexTiny big.Float
+	// Parse truncates to 64 bits if precision is currently 0
+	complexTiny.SetPrec(100000)
+	complexTiny.Parse("0.0000000000000000000000000000000000000"+
+		"12345678901234567890123456789012345678901234567890", 10)
+	complexTiny.SetPrec(complexTiny.MinPrec())
+
+	codec := internal.BigFloatCodec
+	testCodecRoundTrip(t, codec, []testCase[*big.Float]{
+		// example in implementation comments
+		{"seven(3)", newBigFloat(7.0, 0, 3), nil},
+		{"seven(4)", newBigFloat(7.0, 0, 4), nil},
+		{"seven(10)", newBigFloat(7.0, 0, 10), nil},
+		{"-seven(3)", newBigFloat(-7.0, 0, 3), nil},
+		{"-seven(4)", newBigFloat(-7.0, 0, 4), nil},
+		{"-seven(10)", newBigFloat(-7.0, 0, 10), nil},
+
+		{"tiny", newBigFloat(12345.0, -100, 20), nil},
+		{"mixed", newBigFloat(12345.0, -10, 20), nil},
+		{"large", newBigFloat(12345.0, 100, 20), nil},
+		{"-tiny", newBigFloat(-12345.0, -100, 20), nil},
+		{"-mixed", newBigFloat(-12345.0, -10, 20), nil},
+		{"-large", newBigFloat(-12345.0, 100, 20), nil},
+
+		{"-Inf", &negInf, nil},
+		{"+Inf", &posInf, nil},
+		{"-0", &negZero, nil},
+		{"+0", &posZero, nil},
+
+		{"complex whole", &complexWhole, nil},
+		{"complex mixed", &complexMixed, nil},
+		{"complex tiny", &complexTiny, nil},
+	})
+}
+
+func TestBigFloatOrdering(t *testing.T) {
+	var negInf, posInf, negZero, posZero big.Float
+	negInf.SetInf(true)
+	posInf.SetInf(false)
+	negZero.Neg(&negZero)
+	assert.True(t, negZero.Signbit())
+	assert.False(t, posZero.Signbit())
+	assert.Equal(t, 0, negZero.Cmp(&posZero))
+	assert.NotEqual(t, &negZero, &posZero)
+
+	assert.IsIncreasing(t, [][]byte{
+		encodeBigFloat(&negInf),
+
+		// Negative Numbers
+		// for the same matissa, a higher exponent (first) or precision is more negative
+
+		// large negative numbers
+		encodeBigFloat(newBigFloat(-12345.0, 10000, 21)),
+		encodeBigFloat(newBigFloat(-12345.0, 10000, 20)),
+		encodeBigFloat(newBigFloat(-12345.0, 10000, 19)),
+		encodeBigFloat(newBigFloat(-12345.0, 9999, 21)),
+		encodeBigFloat(newBigFloat(-12345.0, 9999, 20)),
+		encodeBigFloat(newBigFloat(-12345.0, 9999, 19)),
+
+		// both whole and fractional parts
+		encodeBigFloat(newBigFloat(-12345.0, 10, 21)),
+		encodeBigFloat(newBigFloat(-12345.0, 10, 20)),
+		encodeBigFloat(newBigFloat(-12345.0, 10, 19)),
+
+		// numbers near -7.0
+		encodeBigFloat(newBigFloat(-7.1, 0, 21)),
+		encodeBigFloat(newBigFloat(-7.1, 0, 20)),
+		encodeBigFloat(newBigFloat(-7.0, 0, 10)), // shift 13
+		encodeBigFloat(newBigFloat(-7.0, 0, 4)),  // shift 5
+		encodeBigFloat(newBigFloat(-7.0, 0, 3)),  // shift 5
+		encodeBigFloat(newBigFloat(-6.9, 0, 21)),
+		encodeBigFloat(newBigFloat(-6.9, 0, 20)),
+
+		// very small negative numbers
+		encodeBigFloat(newBigFloat(-12345.0, -10000, 21)),
+		encodeBigFloat(newBigFloat(-12345.0, -10000, 20)),
+		encodeBigFloat(newBigFloat(-12345.0, -10000, 19)),
+
+		// zeros
+		encodeBigFloat(&negZero),
+		encodeBigFloat(&posZero),
+
+		// Positive Numbers
+		// for the same matissa, a higher exponent (first) or precision is more positive
+
+		// very small positive numbers
+		encodeBigFloat(newBigFloat(12345.0, -10000, 19)),
+		encodeBigFloat(newBigFloat(12345.0, -10000, 20)),
+		encodeBigFloat(newBigFloat(12345.0, -10000, 21)),
+
+		// numbers near 7.0
+		encodeBigFloat(newBigFloat(6.9, 0, 20)),
+		encodeBigFloat(newBigFloat(6.9, 0, 21)),
+		encodeBigFloat(newBigFloat(7.0, 0, 3)),  // shift
+		encodeBigFloat(newBigFloat(7.0, 0, 4)),  // shift 5
+		encodeBigFloat(newBigFloat(7.0, 0, 10)), // shift 13
+		encodeBigFloat(newBigFloat(7.1, 0, 20)),
+		encodeBigFloat(newBigFloat(7.1, 0, 21)),
+
+		// both whole and fractional parts
+		encodeBigFloat(newBigFloat(12345.0, 10, 19)),
+		encodeBigFloat(newBigFloat(12345.0, 10, 20)),
+		encodeBigFloat(newBigFloat(12345.0, 10, 21)),
+
+		// large positive numbers
+		encodeBigFloat(newBigFloat(12345.0, 9999, 19)),
+		encodeBigFloat(newBigFloat(12345.0, 9999, 20)),
+		encodeBigFloat(newBigFloat(12345.0, 9999, 21)),
+		encodeBigFloat(newBigFloat(12345.0, 10000, 19)),
+		encodeBigFloat(newBigFloat(12345.0, 10000, 20)),
+		encodeBigFloat(newBigFloat(12345.0, 10000, 21)),
+
+		encodeBigFloat(&posInf),
+	})
 }
