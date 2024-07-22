3e6f385a191d976bf162564e536cde3e8dbbf07e
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-07-22 14:48:55 -0700

    Add a test for Bytes with a different type def.

--- internal/bytes_test.go
+++ internal/bytes_test.go
@@ -16,3 +16,15 @@ func TestBytes(t *testing.T) {
 	})
 	testCodecFail(t, codec, []byte{0})
 }
+
+func TestBytesUnderlyingType(t *testing.T) {
+	type header []byte
+	codec := internal.MakeBytesCodec[header]()
+	testCodec(t, codec, []testCase[header]{
+		{"nil", header(nil), []byte{pNil}},
+		{"empty", header{}, []byte{empty}},
+		{"[0]", header{0}, []byte{nonEmpty, 0x00}},
+		{"[1, 2, 3]", header{1, 2, 3}, []byte{nonEmpty, 0x01, 0x02, 0x03}},
+	})
+	testCodecFail(t, codec, header{0})
+}
