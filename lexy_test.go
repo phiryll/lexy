ff98d520f1cb613add656f4a0d2d0577e7556aa9
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-08-17 13:57:30 -0700

    Add a test demonstrating some slice gotchas.
    Ref #87
    Ref #91

--- lexy_test.go
+++ lexy_test.go
@@ -16,3 +16,21 @@ func TestBoundsCheck(t *testing.T) {
 		_ = buf[10]
 	})
 }
+
+// Demonstrating a gotcha when working with slices.
+// A slice can be resliced up to it's capacity, regardless of its length.
+func TestIndexingBeyondSliceLength(t *testing.T) {
+	t.Parallel()
+	buf := []byte{0, 1, 2, 3}
+	empty := buf[:0]
+
+	assert.Panics(t, func() {
+		_ = empty[0]
+	})
+	assert.Equal(t, byte(3), (empty[:4])[3])
+
+	assert.Panics(t, func() {
+		_ = empty[1:]
+	})
+	assert.Equal(t, byte(3), (empty[1:4])[2])
+}
