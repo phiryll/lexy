649b861ef1ac31c77cc3c95bc847c38d5ecb61f9
Jennifer Conner <2819667+phiryll@users.noreply.github.com>
2024-08-28 13:00:02 -0700

    Change bench.sh to a different use case, use during development.
    Another shell script will be added to run all the benchmarks.
    
    Ref #103

--- bench.sh
+++ bench.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# This script is for running benchmark tests during development.
+# A typical use would look like:
+#
+#   $ ./bench.sh Float64 3
+#
+# Or if "Float64" unintentionally matches multiple benchmarks:
+#
+#   $ ./bench.sh "BencharkFloat64$" 3
+
 set -e
 
-count=${1:-20}
-tests=${2:-.}
-go test -bench ${tests} -benchmem -timeout 0 -count=${count}
+tests=${1:-.}
+count=${2:-10}
+go test -bench "${tests}" -benchmem -timeout 0 -count=${count}
